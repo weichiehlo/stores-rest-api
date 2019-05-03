a90cac88f0c511257ca7197ce525de72a6ed46d0
Andy Lo <wlo@fortinet-us.com>
2019-05-03 11:47:06 -0700

    Added PostgreSQL and psycopg2

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -8,7 +9,8 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'  #sqlite can be any sql (mysql)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'  #sqlite can be any sql (mysql)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db') # read the environment variable in Heroku if not avaliable, use sqlite
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'andy'
 api = Api(app)
